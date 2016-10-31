33bb03df02a21629ef601c5938bc4e98e235a255
QuadCoreBr <brianr0896@gmail.com>
2016-10-31 07:21:34 -0600

    Totally rewriten 'cause of glitch in the measurements.
    Not that new but changed with the base code again. Still in beta!

--- ArduinoCode/homeStairs/homeStairs.ino
+++ ArduinoCode/homeStairs/homeStairs.ino
@@ -3,7 +3,8 @@
  * //making sense of the Parallax PIR sensor's output
  * //////////////////////////////////////////////////
  *
- * Switches a LED according to the state of the sensors output pin.
+ * Turns on a relay deppending on the motion detected and with a fotoresistor avoids
+ * lightning on when there's no need to. 
  * Determines the beginning and end of continuous motion sequences.
  *
  * @author: Kristian Gohlke / krigoo (_) gmail (_) com / http://krx.at
@@ -28,26 +29,26 @@
 /////////////////////////////
 //VARS
 //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int calibrationTime = 10;        
+int calibrationTime = 15;        
 
 //the time when the sensor outputs a low impulse
 long unsigned int lowIn;         
 
 //the amount of milliseconds the sensor has to be low 
 //before we assume all motion has stopped
-long unsigned int pause = 15000;  
-unsigned int lightMin = 50;
+long unsigned int pause = 12000; 
+//the read value given by the fotoResistor
+int valueMinToOn =  20;
 
-boolean lockLow = false;
+boolean lockLow = true;
 boolean takeLowTime;
-boolean lightRead = true;  
+boolean enoughLightToOn;
+boolean relayState;  
 
 int upperPirPin = 2;
 int bottomPirPin = 3;//the digital pin connected to the PIR sensor's output
 int relayPin = 4;
 int fotoResistor = A0;
-boolean pushStayOn = 5;
-
 
 /////////////////////////////
 //SETUP
@@ -56,12 +57,9 @@ void setup(){
   pinMode(upperPirPin, INPUT);
   pinMode(bottomPirPin, INPUT);
   pinMode(relayPin, OUTPUT);
-  pinMode(pushStayOn,INPUT);
   digitalWrite(upperPirPin, LOW);
   digitalWrite(bottomPirPin,LOW);
   digitalWrite(relayPin,HIGH);
-  
-  pushStayOn=false;
   //give the sensor some time to calibrate
   Serial.print("calibrating sensor ");
     for(int i = 0; i < calibrationTime; i++){
@@ -71,49 +69,46 @@ void setup(){
     Serial.println(" done");
     Serial.println("SENSOR ACTIVE");
     delay(50);
-  
   }
 
 ////////////////////////////
 //LOOP
 void loop(){
-  /*if(digitalRead(pushStayOn)==HIGH){
-    pushStayOn=true;
-    digitalWrite(relayPin,HIGH);
-  }
-  else{
-    pushStayOn=false;
-  }*/
-    if((digitalRead(upperPirPin) == HIGH || digitalRead(bottomPirPin)==HIGH) && pushStayOn==false){
-      Serial.println("UPPER: " +String(digitalRead(upperPirPin)) + " BOTTOM: " + String(digitalRead(bottomPirPin)) + "Resistance Value: "+analogRead(fotoResistor));
-       if(!lockLow && lightRead){  
-         //makes sure we wait for a transition to LOW before any further output is made:
-         lockLow = true;
-         digitalWrite(relayPin, LOW);
-         delay(50);
-         }         
-         takeLowTime = true;
-       }
 
-     if((digitalRead(upperPirPin) == LOW && digitalRead(bottomPirPin)==LOW) && pushStayOn==false){
-      Serial.println("Resistance Value: " + String(analogRead(fotoResistor)));     
-       if(takeLowTime){
-        lowIn = millis();          //save the time of the transition from high to LOW
-        takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+     if(digitalRead(upperPirPin) == HIGH || digitalRead(bottomPirPin)==HIGH){
+       // Serial.println("UPPER: " +String(digitalRead(upperPirPin)) + " BOTTOM: " + String(digitalRead(bottomPirPin)) + " Resistance Value: "+analogRead(fotoResistor));
+       if(lockLow){  
+         //makes sure we wait for a transition to LOW before any further output is made:
+         lockLow = false;            
+         Serial.println("---");
+         Serial.print("motion detected at ");
+         Serial.print(millis()/1000);
+         Serial.println(" sec");
+         if(enoughLightToOn){
+          digitalWrite(relayPin,LOW);
+         }else{
+          Serial.println("Movement but it's not dark");
+         } 
+       }         
+         takeLowTime = true;
+     }
+      if(digitalRead(upperPirPin) == LOW && digitalRead(bottomPirPin)==LOW){       
+        if(takeLowTime){
+          lowIn = millis();          //save the time of the transition from high to LOW
+          takeLowTime = false;       //make sure this is only done at the start of a LOW phase
         }
        //if the sensor is low for more than the given pause, 
        //we assume that no more motion is going to happen
-       if(lockLow && millis() - lowIn > pause){  
+       if(!lockLow && millis() - lowIn > pause){  
            //makes sure this block of code is only executed again after 
            //a new motion sequence has been detected
-           lockLow = false;                        
+           digitalWrite(relayPin,HIGH);
+           delay(100);
+           enoughLightToOn= analogRead(fotoResistor)<=valueMinToOn;
+           lockLow = true;                    
            Serial.print("motion ended at ");      //output
            Serial.print((millis() - pause)/1000);
            Serial.println(" sec");
-           digitalWrite(relayPin, HIGH);
-           delay(100);
-           lightRead = analogRead(fotoResistor)<lightMin;
-           delay(50);
            }
-     }
-}
+       }
+  }
